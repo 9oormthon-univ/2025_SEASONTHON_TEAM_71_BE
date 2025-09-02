a2e6307923a4925c34c8dd6c07e1edcea8a5aacb
T-ferret <hs381717@gmail.com>
2025-09-02 22:02:46 +0900

    api response 통일화

---
+++ src/main/java/goormton/team/gotjob/global/error/InvalidParameterException.java
@@ -0,0 +1,23 @@
+package goormton.team.gotjob.global.error;
+
+import goormton.team.gotjob.global.payload.ErrorCode;
+import lombok.Getter;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Getter
+public class InvalidParameterException extends DefaultException{
+
+    private Errors errors;
+
+    public InvalidParameterException(Errors errors) {
+        super(ErrorCode.INVALID_PARAMETER);
+        this.errors = errors;
+    }
+
+    public List<FieldError> getFieldErrors() {
+        return errors.getFieldErrors();
+    }
+}
