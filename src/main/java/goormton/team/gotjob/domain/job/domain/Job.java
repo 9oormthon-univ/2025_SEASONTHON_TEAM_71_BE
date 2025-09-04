8ae40913f1534ff41f3d142e53b42b28f0b08812
chahyeonmin91 <chahyeonmin91@gmail.com>
2025-09-04 15:12:28 +0900

    feat:job

--- src/main/java/goormton/team/gotjob/domain/job/domain/Job.java
+++ src/main/java/goormton/team/gotjob/domain/job/domain/Job.java
@@ -1,4 +1,27 @@
 package goormton.team.gotjob.domain.job.domain;
 
-public class Job {
+import goormton.team.gotjob.domain.common.BaseEntity;
+import goormton.team.gotjob.domain.company.domain.Company;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity @Table(name="jobs")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Job extends BaseEntity {
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY) @JoinColumn(name="company_id")
+    private Company company;
+
+    @Column(nullable=false) private String title;
+    private String employmentType;
+    private String location;
+    private Integer minSalary;
+    private Integer maxSalary;
+    @Lob private String requirements;
+    @Lob private String description;
+    private String status = "OPEN";
+    private LocalDate deadline;
 }
